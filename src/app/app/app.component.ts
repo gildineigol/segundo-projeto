a21dfe42c8b715004d9c50842768322876ac5bc6
Gildinei <neim-neim@hotmail.com>
2020-07-12 14:41:34 -0300

    criação array

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Olá Maynara, parabéns pelo seu dia, feliz dia da mulher ';
+  title = 'Olá Maynara';
 }
--- src/app/meu-primeiro2/meu-primeiro2.component.ts
+++ src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -7,5 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeuPrimeiro2Component {
 
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular', 'HTML']
+
+  constructor(){
+    this.nomePortal = 'hhtp://youtube.com.br';
+
+    for (let i=0; i<this.cursos.length; i++){
+      let cursos = this.cursos[i];
+    }
+
+  }
+
 
 }
