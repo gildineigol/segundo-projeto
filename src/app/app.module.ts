67b2d143b0d385360ee826f455067aee26142ddb
Gildinei <neim-neim@hotmail.com>
2020-07-11 23:31:31 -0300

    alterações de pasta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './principal/app.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 
